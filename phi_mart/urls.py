b65cf8093ae1c0008e6633674ce2adecc4f4ceb9
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-12 16:58:59 +0600

    Review model have some changes

--- phi_mart/urls.py
+++ phi_mart/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/',include('api.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
 ]
